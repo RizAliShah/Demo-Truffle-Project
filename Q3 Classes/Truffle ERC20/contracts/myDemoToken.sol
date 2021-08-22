3320dce521143cd26c4cc5f7d286ea909871bc1d
AliWisam <aliwisaam73@gmail.com>
2021-08-23 01:13:50 +0500

    setting path and added openzeppelin

--- Q3 Classes/Truffle ERC20/contracts/myDemoToken.sol
+++ Q3 Classes/Truffle ERC20/contracts/myDemoToken.sol
@@ -1,16 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 contract myDemoToken is ERC20, Ownable, ERC20Burnable, Pausable{
     using SafeMath for uint;
     uint public cap;
     
-    constructor() ERC20("myDemoToken", "MDT") public{
+    constructor() ERC20("myDemoToken", "MDT"){
         uint initialSupply = 10000 * (10 ** decimals());
         cap = initialSupply.mul(2);
         _mint(msg.sender, initialSupply);
@@ -19,15 +20,15 @@ contract myDemoToken is ERC20, Ownable, ERC20Burnable, Pausable{
     function generateTokens(address account, uint amount) public whenNotPaused onlyOwner{
         require(account != address(0), "Invalid address");
         require(amount > 0, "Invalid Amount");
-        require(totalSupply().add(amount) < cap, "Overlimit Token: Token generation failed!");
+        require(totalSupply().add(amount) < cap, "Overlimit Token: Token     generation failed!");
         _mint(account, amount);
     }
     
-    function stopTransaction() public whenNotPaused onlyOwner{
-        _pause;
-    }
+    // function stopTransaction() public whenNotPaused onlyOwner{
+    //     _pause;
+    // }
     
-    function startTransaction() public whenPaused onlyOwner{
-        _unpause;
-    }
+    // function startTransaction() public whenPaused onlyOwner{
+    //     _unpause;
+    // }
 }
